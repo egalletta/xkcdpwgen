5cf75a66a5239e57d0d583646882e5eeebf7a240
Enzo Galletta <egalletta@login-students.ccs.neu.edu>
2020-02-19 21:11:33 -0500

    pwgen

--- xkcdpwgen.py
+++ xkcdpwgen.py
@@ -6,12 +6,12 @@ with open('words.txt', 'r') as f:
     wordlist = f.readlines()
 
 nums = range(0, 9)
-
+symbs = ["~,", "!",  "@", "#", "$", "%", "^", "&", "*", ";", ":"]
 parser = argparse.ArgumentParser()
 parser.add_argument("-w", "--words", type=int, help="include WORDS words in the password", default=4)
-parser.add_argument("-c", "--caps", help="capitalize the first letter of CAPS random words", default=0)
-parser.add_argument("-n", "--numbers", help="insert NUMBERS random numbers in the password", default=0)
-parser.add_argument("-s", "--symbols", help="insert SYMBOLS random symbols in the password", default=0)
+parser.add_argument("-c", "--caps", type=int, help="capitalize the first letter of CAPS random words", default=0)
+parser.add_argument("-n", "--numbers", type=int, help="insert NUMBERS random numbers in the password", default=0)
+parser.add_argument("-s", "--symbols", type=int, help="insert SYMBOLS random symbols in the password", default=0)
 args = parser.parse_args()
 
 password = []
@@ -19,8 +19,14 @@ password = []
 for num_words in range(args.words):
     password.append(secrets.choice(wordlist).replace("\n", ""))
 
+cap_indexes = random.sample(range(0, len(password)), args.caps)
+for i in cap_indexes:
+    password[i] = password[i].capitalize()
+
 for num_nums in range(args.numbers):
     password.insert(random.randint(0, len(password)), str(secrets.choice(nums)))
 
-print("".join(password))
+for num_symbs in range(args.symbols):
+    password.insert(random.randint(0,len(password)), secrets.choice(symbs))
 
+print("".join(password))
